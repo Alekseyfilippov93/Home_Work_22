aff5220c10437df7778cf6d349808f2da6cc47fc
Aleksey40000 <alekseyfilippov9392@gmail.com>
2025-12-17 03:24:20 +0700

    Админка

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from catalog.models import Category, Product
 
-# Register your models here.
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    # Отображение id и наименования в списке
+    list_display = ('id', 'name',)
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    # Колонки, которые будут видны в списке товаров
+    list_display = ('id', 'name', 'price', 'category',)
+
+    # Фильтрация по категории (справа появится панель фильтров)
+    list_filter = ('category',)
+
+    # Поиск по названию и описанию
+    search_fields = ('name', 'description',)
