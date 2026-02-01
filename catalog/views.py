23f5be3d84d203e52f09aff2776cce69c19af087
Aleksey40000 <alekseyfilippov9392@gmail.com>
2026-02-02 02:10:29 +0700

    Кеширование и бизнес логика

--- catalog/views.py
+++ catalog/views.py
@@ -10,11 +10,33 @@ from django.views.generic import (
 from catalog.models import Product
 from catalog.forms import ProductForm
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from catalog.services import get_products_by_category
+from catalog.models import Category
+from django.core.cache import cache
+from config import settings
 
 
 class ProductListView(ListView):
     model = Product
     template_name = "catalog/home.html"
+
+    def get_queryset(self):
+        """Реализация низкоуровневого кеширования списка продуктов"""
+        # Сначала фильтруем только опубликованные
+        queryset = super().get_queryset().filter(is_published=True)
+
+        # Если кеш включен в settings.py
+        if settings.CACHE_ENABLED:
+            key = "all_products_list"  # Ключ для хранения в Redis
+            cache_data = cache.get(key)
+
+            if cache_data is None:
+                # Если в кеше пусто, сохраняем туда QuerySet
+                cache_data = queryset
+                cache.set(key, cache_data, 60 * 15)  # Кешируем на 15 минут
+            return cache_data
+
+        return queryset
 
 
 class ProductDetailView(DetailView, LoginRequiredMixin):
@@ -73,3 +95,20 @@ class ProductDeleteView(DeleteView, LoginRequiredMixin, UserPassesTestMixin):
         if product.owner == user or user.has_perm("catalog.delete_product"):
             return True
         return False
+
+
+class CategoryProductListView(ListView):
+    model = Product
+    template_name = "catalog/category_products.html"
+    context_object_name = "products"
+
+    def get_queryset(self):
+        # Получаем id категории из URL и вызываем наш сервис
+        category_id = self.kwargs.get("pk")
+        return get_products_by_category(category_id)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        # Добавим название категории в заголовок страницы
+        context["category"] = Category.objects.get(pk=self.kwargs.get("pk"))
+        return context
